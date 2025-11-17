89475bbf1a27f32cf799253a9579eb6342472754
chafik mouad <mouadchafik1301@gmail.com>
2025-11-17 12:20:57 +0100

    Convert tp-blog submodule into normal folder

---
+++ tp-blog/src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/add-post">Add Post</router-link>
+    </nav>
+</template>
+
+<style>
+    .navbar {
+        background: blue;
+        gap:20px;
+        display: flex;
+        padding: 10px;
+    }    
+</style>
